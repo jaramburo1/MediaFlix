983d3889e8bf59388719f3edd51c414abd2a7b70
jaramburo1 <josepharamburo.gwie@gmail.com>
2018-08-22 17:05:13 -0700

    adding twitter search function for 1st test

---
+++ twitter API/auth.js
@@ -0,0 +1,15 @@
+<script>
+  window.dmAsyncInit = function()
+  {
+    DM.init({ apiKey: '28609fecadb89fda8f7c', status: true, cookie: true });
+  };
+  (function() {
+    var e = document.createElement('script');
+    e.async = true;
+    e.src = 'https://api.dmcdn.net/all.js';
+
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(e, s);
+	handleAPILoaded();
+  }());
+</script>

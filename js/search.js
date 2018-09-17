d2c853ad1fb87f7d964bc57b205f1e8e23ca9231
jaramburo1 <josepharamburo.gwie@gmail.com>
2018-09-16 21:55:54 -0700

    setup before merging youtube and dailymotion video searches

--- js/search.js
+++ js/search.js
@@ -46,7 +46,55 @@ var videoIDs = '';
     for (var i = 0; i < response.items.length; i++) {
       var vid = response.items[i];
 	  videos[i].duration = vid.contentDetails.duration;
-      $('#results-container').append(
+      document.write(// After the API loads, call a function to enable the search box.
+function handleAPILoaded() {
+  $('#search-button').attr('disabled', false);
+}
+var video = {
+	id:'',
+	title:'',
+	videoURL:'',
+	thumbnail:'',
+	description:'',
+	duration:''
+};
+var videos = [];
+// Search for a specified string.
+function search() {
+  var q = $('#query').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet',
+    maxResults: 20,
+    type: "video"
+  });
+var videoIDs = '';
+  request.execute(function (response) {
+   // var str = JSON.stringify(response.result);
+    
+    $('#results-container').empty();
+    for (var i = 0; i < response.result.items.length; i++) {
+      var vid = response.result.items[i];
+	  vidURL = 'https://www.youtube.com/embed/' + vid.id.videoID;
+	  video = {vid.id.videoID,vid.snippet.title,vidURL,vid.snippet.description,vid.snippet.thumbnails[medium].url,''};
+	  videos.push(video);
+      if (i == response.result.items.length) { videoIDs += response.result.items[i].id.videoId; }
+      else {
+        videoIDs += response.result.items[i].id.videoId + ",";
+      }
+    }
+    request = gapi.client.request({
+          'method': 'GET',
+          'path': '/youtube/v3/videos',
+          'params':  {'id': videoIDs,
+                 'part': 'snippet,contentDetails,statistics'}
+      });
+      request.execute(function(response){
+    $('#results-container').empty();
+    for (var i = 0; i < response.items.length; i++) {
+      var vid = response.items[i];
+	  videos[i].duration = vid.contentDetails.duration;
+      document.write($('#results-container').append(
         `<div data-video="${video[i]}"
         onclick='playVideo(this)'>
         <figure id='figure-search-results'>
@@ -54,7 +102,36 @@ var videoIDs = '';
         <figcaption><h3>${video.snippet.title}</h3>${video.snippet.description} (${video.contentDetails.duration})</figcaption>
         </figure>
         </div>`
-        );
+        ));
+    }
+    });
+  });
+}
+
+function playVideo(vid){
+  var vidURL = vid.videoURL;
+  var vidDescription = vid.videoURL;
+  console.log("in playVideo funtion",vidURL);
+  $('#player').html("<iframe width='560' height='315' src='https://www.youtube.com/embed/" + vidURL + "?autoplay=1' frameborder='0' allowfullscreen />");
+  $('#feeds').html("<p>" + vidDescription + "</p>");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+);
     }
     });
   });

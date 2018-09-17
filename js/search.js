e0504b687b30180da362e2a668ccefa8af020bbe
jaramburo1 <josepharamburo.gwie@gmail.com>
2018-09-17 00:08:47 -0700

    setup before merging youtube and dailymotion video searches

--- js/search.js
+++ js/search.js
@@ -2,7 +2,15 @@
 function handleAPILoaded() {
   $('#search-button').attr('disabled', false);
 }
-
+var video = {
+	id:'',
+	title:'',
+	videoURL:'',
+	thumbnail:'',
+	description:'',
+	duration:''
+};
+var videos = [];
 // Search for a specified string.
 function search() {
   var q = $('#query').val();
@@ -17,6 +25,12 @@ var videoIDs = '';
    // var str = JSON.stringify(response.result);
     
     $('#results-container').empty();
+	
+	var vid = response.result.items[i];
+	  vidURL = 'https://www.youtube.com/embed/' + vid.id.videoID;
+	  video = {vid.id.videoID,vid.snippet.title,vidURL,vid.snippet.description,vid.snippet.thumbnails[medium].url,''};
+	  videos.push(video);
+	
     for (var i = 0; i < response.result.items.length; i++) {
       //var video = response.result.items[i];
       if (i == response.result.items.length) { videoIDs += response.result.items[i].id.videoId; }
@@ -33,13 +47,14 @@ var videoIDs = '';
       request.execute(function(response){
     $('#results-container').empty();
     for (var i = 0; i < response.items.length; i++) {
-      var video = response.items[i];
+      vid = response.items[i];
+	  videos[i].duration = vid.contentDetails.duration;
       $('#results-container').append(
-        "<div data-url='" + response.items[i].id +
+        "<div data-url='" + videos[i].id +
         "'onclick='playVideo(this)'>" +
         "<figure id='figure-search-results'>" +
-        "<img src='" + video.snippet.thumbnails.medium.url + "' />" +
-        "<figcaption>" + "<h3>" + video.snippet.title + "</h3>" + video.snippet.description + "(" + video.contentDetails.duration + ")" + "</figcaption>" +
+        "<img src='" + videos[i].thumbnail + "' />" +
+        "<figcaption>" + "<h3>" + videos[i].title + "</h3>" + videos[i].description + "(" + videos[i].duration + ")" + "</figcaption>" +
         "</figure>" +
         "</div>"
         );
@@ -50,6 +65,8 @@ var videoIDs = '';
 
 function playVideo(vid){
   var vidURL = vid.getAttribute("data-url");
+  desc = videos.find((elem)=>{elem.id=vid});
   console.log("in playVideo funtion",vidURL);
   $('#player').html("<iframe width='560' height='315' src='https://www.youtube.com/embed/" + vidURL + "?autoplay=1' frameborder='0' allowfullscreen />");
+  $('#feeds').html(desc);
 }
